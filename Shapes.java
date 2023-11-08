ba108b2dd8bebec3d2bdabbfadf040b146d4da02
James <145038163+SteelSinter@users.noreply.github.com>
2023-11-08 14:50:35 -0700

    Add files via upload

--- Shapes.java
+++ Shapes.java
@@ -145,6 +145,22 @@ class Rectangle extends GeometricObject {
 		this.width = width;
 		this.height = height;
 	}
+	
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public double getWidth() {
+		return width;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
 }
 
 class Triangle extends GeometricObject implements Comparable<Triangle> {
